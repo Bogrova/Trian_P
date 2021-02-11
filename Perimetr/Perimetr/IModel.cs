4f7f00047d7686911acfc9f3cbb17613902720f4
Bogrova <75212894+Bogrova@users.noreply.github.com>
2021-02-11 20:47:33 +0300

    Так

---
+++ Perimetr/Perimetr/IModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perimetr
+{
+    interface IModel
+    {
+        /// <summary>
+        /// Метод возвращающий периметр по двум сторонам
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        int GetP(int a,int b);
+    }
+}
